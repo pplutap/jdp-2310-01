77d384fa2149f91a1a71e2d0ce439c9739f4571c
DamianNu <damian21v@gmail.com>
2023-09-22 18:31:35 +0200

    product controller implementation, adding Lombok

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,39 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "productId")
+    public ProductDto getProduct(Long ProductId) {
+        return new ProductDto(0005L, "Czapka", "Czapka z daszkiem", 100L);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "productId")
+    public void deleteProduct(Long ProductId) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "")
+    public ProductDto updateProduct(ProductDto productDto) {
+        return new ProductDto(0005L, "CzapkaX", "Czapka z daszkiem z nadrukiem X", 110L);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "")
+    public void createProduct(ProductDto productDto) {
+
+    }
 }
--- src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/ProductDto.java
@@ -1,2 +1,15 @@
-package com.kodilla.ecommercee.domain;public class ProductDto {
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ProductDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Long price;
+
 }

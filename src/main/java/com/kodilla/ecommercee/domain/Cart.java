9791dffbe50126a5ef01a1be7b0c9e775b8f6c79
DamianNu <damian21v@gmail.com>
2023-09-23 12:30:58 +0200

    CartController - initial endpoint implementation

---
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -0,0 +1,15 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Cart {
+    private Long id;
+    private List<ProductDto> listProduct;
+}

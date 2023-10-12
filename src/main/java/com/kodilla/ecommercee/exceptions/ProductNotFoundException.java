b7b71fc62275ded42eebb0b12d2ebb60cc6c00f3
DamianNu <damian21v@gmail.com>
2023-10-12 17:56:45 +0200

    ProductController - rzeczywista implementacja metod

---
+++ src/main/java/com/kodilla/ecommercee/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.exceptions;
+
+public class ProductNotFoundException extends Exception {
+    public ProductNotFoundException(String string) {
+
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -0,0 +1,46 @@
+package com.kodilla.ecommercee.mapper;
+
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductMapper {
+
+    public Product mapToProduct(ProductDto productDto) {
+        return new Product(
+                productDto.getId(),
+                productDto.getName(),
+                productDto.getDescription(),
+                productDto.getPrice(),
+                productDto.getCarts(),
+                productDto.getGroup()
+        );
+    }
+
+    public ProductDto mapToProductDto(Product product) {
+        return new ProductDto(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getCarts(),
+                product.getGroup()
+        );
+    }
+
+    public List<Product> mapToProductList(final List<ProductDto> productList) {
+        return productList.stream()
+                .map(this::mapToProduct)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductDto> mapToProductDtoList(final List<Product> productList) {
+        return productList.stream()
+                .map(this::mapToProductDto)
+                .collect(Collectors.toList());
+    }
+}

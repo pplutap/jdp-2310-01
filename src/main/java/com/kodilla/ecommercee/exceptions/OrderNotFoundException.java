993d5bfbfcf021e773948ef6b2b74c397230b7e0
DamianNu <damian21v@gmail.com>
2023-10-15 11:59:28 +0200

    praca nad taskiem JDP231001-24

---
+++ src/main/java/com/kodilla/ecommercee/exceptions/OrderNotFoundException.java
@@ -0,0 +1,6 @@
+package com.kodilla.ecommercee.exceptions;
+
+public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String string) {
+    }
+}
---
+++ src/main/java/com/kodilla/ecommercee/service/OrderService.java
@@ -0,0 +1,13 @@
+package com.kodilla.ecommercee.service;
+
+import com.kodilla.ecommercee.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+}
